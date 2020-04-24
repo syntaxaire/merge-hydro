990083fc0f5c6752a4fa69d88a362d00c95ff43b
syntaxaire <syntaxaire@gmail.com>
2020-04-24 16:33:32 -0400

    Refactor entire project, add type annotations

--- process.py
+++ process.py
@@ -1,43 +1,47 @@
 # Process Hydro sheets to meter readings
 
-import os
-import re
-import xlrd
-import logging
-import pandas as pd
 import configparser
+import logging
+import xlrd
+from pathlib import Path
+from typing import Generator, Tuple
+
 import numpy as np
+import pandas as pd
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(message)s")
 
-cwd = os.getcwd()
-print(f"Current working directory is {cwd}")
+XLS_FLOAT_TYPE = 2
+
+converters = {"Account Number": int}
 
 
-def find_in_sheet(val, sheet: xlrd.sheet.Sheet):
-    """Return a tuple containing the (row, col) of first match searching row 0, then row 1, etc.
-    Return false if not found."""
+def find_in_sheet(val, sheet: xlrd.sheet.Sheet) -> Tuple[int, int]:
+    """Return a tuple containing the (row, col) of first match searching row 0, then row 1, etc."""
     for row in range(sheet.nrows):
         for col in range(sheet.ncols):
             if sheet.cell_value(row, col) == val:
                 return row, col
-    return False
+    raise LookupError(f'Value {val} not found in sheet {sheet}')
 
 
-def get_billing_lines(sheet: xlrd.sheet.Sheet):
+def get_billing_lines(sheet: xlrd.sheet.Sheet) -> int:
     """Take an xl sheet with a Hydro One bill as input and return the number of billing lines (accounts)"""
     lines = 0
-    row, col = find_in_sheet("Line #", sheet)  # this cell should be unique
+    try:
+        row, col = find_in_sheet("Line #", sheet)  # this cell should be unique
+    except LookupError:
+        raise ValueError(f'{sheet} does not appear to be a Hydro One bill (no "Line #" cell)')
 
     # find number of lines in bill by walking down
     row += 1
-    while sheet.cell_type(row, col) == 2:  # Excel float cell type
+    while sheet.cell_type(row, col) == XLS_FLOAT_TYPE:
         row += 1
         lines += 1
     return lines
 
 
-def get_bill(filename: str) -> pd.DataFrame:
+def get_bill_dataframe(filename: Path) -> pd.DataFrame:
     """Take the name of an Excel file containing a Hydro One bill and return a Pandas dataframe
     constructed from a select area of the invoice sheet."""
 
@@ -45,7 +49,7 @@ def get_bill(filename: str) -> pd.DataFrame:
     with xlrd.open_workbook(filename=filename) as xl:
         try:
             sheet = xl.sheet_by_name("Invoice Summary")  # this sheet contains the bill
-        except:
+        except xlrd.biffh.XLRDError:
             logging.error(f"Not able to open sheet \"Invoice Summary\" in {filename}")
             raise
 
@@ -62,59 +66,72 @@ def get_bill(filename: str) -> pd.DataFrame:
         return pd.read_excel(io=filename,
                              sheet_name="Invoice Summary",
                              header=label_row,
-                             skip_footer=footer_size,
+                             skipfooter=footer_size,
                              index_col=0,
+                             converters=converters,
                              usecols=list(range(1, last_col + 1)))
 
 
-config_file = "process.cfg"
-config = configparser.ConfigParser(allow_no_value=True)
-config.read(config_file)
+def process(spreadsheets: Generator[Path, None, None], config) -> pd.DataFrame:
+    bills = [get_bill_dataframe(filename) for filename in spreadsheets]
+    logging.debug("Completed loading dataframes from Excel.")
+    mass_df = pd.concat(bills)  # join all bills in preparation for splitting by account number
+    # convert date columns from strings
+    mass_df['Reading From Date'] = pd.to_datetime(mass_df['Reading From Date'])
+    mass_df['Reading From Date'] = [dt.date() for dt in mass_df['Reading From Date']]
+    mass_df['Reading To Date'] = pd.to_datetime(mass_df['Reading To Date'])
+    mass_df['Reading To Date'] = [dt.date() for dt in mass_df['Reading To Date']]
 
-using_aliases = False
-if "Aliases" in config:
-    using_aliases = True
-    aliases = {}
-    for account in config["Aliases"]:
-        aliases[account] = config["Aliases"][account]
+    # new columns
+    mass_df['Days In Reading'] = mass_df['Reading To Date'] - mass_df['Reading From Date']
+    mass_df['Days In Reading'] = mass_df['Days In Reading'] / np.timedelta64(1, 'D')
+    mass_df['kWh Per Day'] = mass_df['Metered Usage [kWh]'] / mass_df['Days In Reading']
+
+    # drop certain types of rows: unbilled entries, and sentinel lights
+    mass_df = mass_df[mass_df["Service Classification"] != "Sentinel Lights"]
+    mass_df = mass_df[mass_df["Reason Not Billed"] != "No billing as of summary billing cut off date"]
+
+    # after processing, drop columns according to config
+    if "Drop" in config:
+        for drop_label in mass_df.columns.values.tolist():
+            if drop_label.lower() in config["Drop"]:
+                mass_df = mass_df.drop(labels=drop_label, axis=1)
+                logging.info(f"Dropped column \"{drop_label}\" according to config")
+
+    return mass_df
+
+
+def write_output(mass_df: pd.DataFrame, aliases: dict):
+    accounts = set(mass_df.index)  # unique account numbers
+
+    # write a spreadsheet with processed results
+    with pd.ExcelWriter("output.xlsx") as writer:
+        for account_num in accounts:
+            if str(account_num) in aliases:
+                worksheet_name = aliases[str(account_num)]
+            else:
+                worksheet_name = str(account_num)
+            mass_df.loc[account_num].to_excel(writer, worksheet_name)
+            logging.info(f"Wrote results for account {account_num} to output.xlsx in sheet {worksheet_name}")
+
+
+def main():
+    cwd = Path.cwd()
+    config_file = cwd / "process.cfg"
+    if not config_file.is_file():
+        print(f"{config_file} not found. Please copy the example config to this file and edit it.")
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read(config_file)
+    if "Aliases" in config:
+        aliases = dict(config["Aliases"])
+    else:
+        aliases = {}
     logging.debug(f"Account aliases: {aliases}")
+    spreadsheets = cwd.glob("*.xls")
+    logging.debug(f"Files to process: {spreadsheets}")
+    mass_df = process(spreadsheets, config)
+    write_output(mass_df, aliases)
 
-spreadsheets = []
-files = os.listdir(path=".")
 
-for file in files:
-    if re.match("^[0-9]{8}.xls", file):
-        spreadsheets.append(file)
-logging.debug(f"Files to process: {spreadsheets}")
-bills_list = [get_bill(filename) for filename in spreadsheets]
-mass_df = pd.concat(bills_list)  # join all bills in preparation for splitting by account number
-mass_df['Reading From Date'] = pd.to_datetime(mass_df['Reading From Date'])  # convert date columns from strings
-mass_df['Reading From Date'] = [dt.to_datetime().date() for dt in mass_df['Reading From Date']]
-mass_df['Reading To Date'] = pd.to_datetime(mass_df['Reading To Date'])
-mass_df['Reading To Date'] = [dt.to_datetime().date() for dt in mass_df['Reading To Date']]
-mass_df['Days In Reading'] = mass_df['Reading To Date'] - mass_df['Reading From Date']
-mass_df['Days In Reading'] = mass_df['Days In Reading'] / np.timedelta64(1, 'D')
-mass_df['kWh Per Day'] = mass_df['Metered Usage [kWh]'] / mass_df['Days In Reading']
-
-# drop certain types of rows: unbilled entries, and sentinel lights
-mass_df = mass_df[mass_df["Service Classification"] != "Sentinel Lights"]
-mass_df = mass_df[mass_df["Reason Not Billed"] != "No billing as of summary billing cut off date"]
-
-# after processing, drop columns according to config
-if "Drop" in config:
-    for drop_label in mass_df.columns.values.tolist():
-        if drop_label.lower() in config["Drop"]:
-            mass_df = mass_df.drop(labels=drop_label, axis=1)
-            logging.info(f"Dropped column \"{drop_label}\" according to config")
-
-accounts = set(mass_df.index)  # unique account numbers
-
-# write a spreadsheet with processed results
-with pd.ExcelWriter("output.xlsx") as writer:
-    for account_num in accounts:
-        if using_aliases:
-            worksheet_name = aliases[str(account_num)]
-        else:
-            worksheet_name = str(account_num)
-        mass_df.loc[account_num].to_excel(writer, worksheet_name)
-        logging.info(f"Wrote results for account {account_num} to output.xlsx in sheet {worksheet_name}")
+if __name__ == "__main__":
+    main()
